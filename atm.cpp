bb3e44709f5547da5b3aa8ff703533f78f9c8338
khalit23 <Touchmate7!>
2022-08-16 07:08:03 +0100

    Feat: created ATM class along with methods

--- atm.cpp
+++ atm.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+class ATM{
+    private:
+    string name;
+    string DOB;
+    int account_number;
+    int balance;
+
+    public:
+    void menu();
+    void register_account();
+    void deposit_amount();
+    void withdraw_amount();
+    void exit();
+    string return_account_details();
+    int return_account_balance();
+};
+
+int main() {
+
+    return 0;
+}
