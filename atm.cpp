ab9b425a22f4279425d4da652cf93e0739e46a81
khalit23 <Touchmate7!>
2022-08-17 06:13:22 +0100

    FEATURE: added the ability to register a bank account and also exit from the ATM menu

--- atm.cpp
+++ atm.cpp
@@ -17,6 +17,7 @@ class ATM{
     void register_account();
     void deposit_amount();
     void withdraw_amount();
+    void exit_option(); //Used to ask the user if they wish to return to menu or exit after carrying out a task
     void exit();
     string return_account_details();
     int return_account_balance();
@@ -34,7 +35,42 @@ void ATM::menu(){
     int option; //Used to store the menu selection of the user
     cout << "Please select an option: "; cin >> option;
 
+    switch (option)
+    {
+    case 1:
+        register_account();
+        break;
+    
+    default:
+        break;
+    }
+
 } 
+
+void ATM::exit(){
+    cout << "We hope to see you again soon!" << endl;
+}
+
+void ATM::exit_option(){
+    string choice;
+    cout << "Would you like to return to the main menu? Type 'yes' or 'no': "; cin >> choice;
+    if (choice == "yes")
+    {
+        menu();
+    }
+    else{
+        exit();
+    }
+}
+
+void ATM::register_account(){
+    cout << "What is your name: "; cin >> name;
+    cout << "What is your date of birth: "; cin >> DOB;
+    cout << "Enter your account number: "; cin >>account_number;
+    balance = 0;
+    cout << "Your account has successfully been created and your balance is set to £0" << endl;
+    exit_option();
+}
 
 int main() {
     ATM atm;
